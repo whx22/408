08338767f9a70fa04f35dcad618b0ee08759542f
whx <2019120573@jou.edu.cn>
2024-05-19 09:03:47 +0800

    3

---
+++ bilibili_dswwl666/3h.cc
@@ -0,0 +1,31 @@
+// brute force algorithm
+// time complexity:   O(n^3)
+// space complexity:  O(n)
+int solution1(int A[], int B[], int C[], int n) {
+  int cnt = 0;
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      for (int k = 0; k < n; ++k) {
+        if (A[i] < B[j] && B[j] < C[k])
+          ++cnt;
+      }
+    }
+  }
+  return cnt;
+}
+
+void quicksort(int q[], int l, int r);
+int search(int q[], int n, int v);
+
+int solution2(int A[], int B[], int C[], int n) {
+  int cnt = 0, p, q;
+  quicksort(A, 0, n - 1);
+  quicksort(B, 0, n - 1);
+  quicksort(C, 0, n - 1);
+  for (int i = 0; i < n; ++i) {
+    p = search(A, n, B[i]);
+    q = search(C, n, B[i]);
+    cnt += p * q;
+  }
+  return cnt;
+}
