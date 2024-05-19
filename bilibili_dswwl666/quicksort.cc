d0c3589f78bc7230e9306c81f09b7f178abd598b
whx <2019120573@jou.edu.cn>
2024-05-19 08:41:38 +0800

    quick sort

--- bilibili_dswwl666/quicksort.cc
+++ bilibili_dswwl666/quicksort.cc
@@ -1,10 +1,17 @@
 void quicksort(int q[], int l, int r) {
-  if (l >= r) return;
+  if (l >= r)
+    return;
   int i = l - 1, j = r + 1, x = q[l + r >> 1];
   while (i < j) {
-    do ++i; while (q[i] < x);
-    do --j; while (q[j] > x);
-    if (i < j) q[i] ^= q[j] ^= q[i] ^= q[j];
+    do
+      ++i;
+    while (q[i] < x);
+    do
+      --j;
+    while (q[j] > x);
+    if (i < j)
+      q[i] ^= q[j] ^= q[i] ^= q[j];
   }
-  quicksort(q, l, j); quicksort(q, j + 1, r);
+  quicksort(q, l, j);
+  quicksort(q, j + 1, r);
 }
