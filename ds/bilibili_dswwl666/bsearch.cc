787a59623342111e793ac42764c7e365111fb00b
whx <2019120573@jou.edu.cn>
2024-06-04 20:58:58 +0800

    2024-06-04

--- ds/bilibili_dswwl666/bsearch.cc
+++ ds/bilibili_dswwl666/bsearch.cc
@@ -1,23 +1,16 @@
-
-/**
- * @brief check if x is the answer
- * 
- * @param x 
- * @return true 
- * @return false 
- */
-bool check(int x) {
-  if (x == 0) return true;
-  else return false;
+void quick_sort(int q[], int l, int r) {
+  if (l >= r) return;
+  int i = l - 1, j = r + 1, x = q[l+r >> 1];
+  while (i < j) {
+    do i++; while (q[i] < x);
+    do j--; while (q[j] > x);
+    if (i < j) q[i] ^= q[j] ^= q[i] ^= q[j];
+  }
+  quick_sort(q, l, j); quick_sort(q, j+1, r);
 }
 
-/**
- * @brief 
- * 
- * @param l 
- * @param r 
- * @return int 
- */
+bool check(int x);
+
 int bsearch_1(int l, int r) {
   while (l < r) {
     int mid = l + r >> 1;
--- ds/bilibili_dswwl666/t_bsearch.cc
+++ ds/bilibili_dswwl666/t_bsearch.cc
@@ -1,4 +1,23 @@
 /**
+ * @brief 整数二分查找：查找划分区间的左右边界
+ * 
+ * 思想：确保每次迭代后，答案都在区间中，当区间长度为1时，即为答案
+ *  
+ * | 满足条件的区间 | | 不满足条件的区间 | 
+ * 1. 查询满足调见的左边界：[l, r] -> [l, mid] 和 [mid + 1, r]
+ *  mid = (l + r) / 2 
+ *  if (check(mid))
+ *    true: r = mid; (to [l, mid])
+ *    false: l = mid + 1; (to [mid + 1, r])
+ * 
+ * 2. 查询满足条件的右边界：[l, r] -> [l, mid - 1] 和 [mid, r] 
+ *  mid = (l + r + 1) / 2
+ *  if (check(mid))
+ *    true: l = mid; (to [mid, r])
+ *    false: r = mid - 1; (to [l, mid - 1])
+ */
+
+/**
  * @brief 检查x是否满足某种性质
  * 
  * @param x 
@@ -10,8 +29,7 @@ bool check(int x) {
 }
 
 /**
- * @brief 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用 
- * 
+ * @brief 区间[l, r]被划分成[l, mid]和[mid + 1, r]时使用
  * @param l 
  * @param r 
  * @return int 
@@ -27,7 +45,6 @@ int bsearch_1(int l, int r) {
 
 /**
  * @brief 区间[l, r]被划分为[l, mid - 1]和[mid, r]时使用
- * 
  * @param l 
  * @param r 
  * @return int 
